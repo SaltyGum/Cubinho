73e7a558121e43cc6b1fb66d12e7c99058398184
Daniel Vargas <daniel_v93@hotmail.com>
2023-04-17 08:33:29 -0300

    not sprite, texture
    bagulho 'e TEXTURA

--- draw.c
+++ draw.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   draw.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jeluiz4 <jeffluiz97@gmail.com>             +#+  +:+       +#+        */
+/*   By: dvargas < dvargas@student.42.rio>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 12:27:00 by jeluiz4           #+#    #+#             */
-/*   Updated: 2023/04/16 12:33:41 by jeluiz4          ###   ########.fr       */
+/*   Updated: 2023/04/17 08:33:17 by dvargas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,51 +95,49 @@ void	generate3d_projection(t_cub3d *blk)
 			my_mlx_pixelput(blk, i, y, blk->map->ceil);
 			y++;}
 
-		int textureOffsetX;
+		int texture_offset_x;
         if (blk->ray[i].is_hit_vertical)
-            textureOffsetX = (int)blk->ray[i].hit_y_wall % TEXTURE_HEIGHT;
+            texture_offset_x = (int)blk->ray[i].hit_y_wall % TEXTURE_HEIGHT;
         else
-            textureOffsetX = (int)blk->ray[i].hit_x_wall % TEXTURE_WIDTH;
+            texture_offset_x = (int)blk->ray[i].hit_x_wall % TEXTURE_WIDTH;
 		//WallColors
-		// wall_bot_pix = wall_bot_pix > HEIGHT ? HEIGHT : wall_bot_pix;
-		// render the wall from wall_top_pix to wall_bot_pix
 		y = wall_top_pix;
 		while (y < wall_bot_pix)
 		{
 			int			dist_from_top;
-			int			sprite_offset_y;
+			int			texture_offset_y;
 			u_int32_t	texel_color;
 
 			dist_from_top = y + (wall_strip_height / 2) - (HEIGHT / 2);
-			sprite_offset_y = dist_from_top
+			texture_offset_y = dist_from_top
 				* ((float)TEXTURE_HEIGHT / wall_strip_height);
 
 			if (blk->ray[i].is_hit_vertical
 				&& blk->ray[i].is_ray_face_right)
 			{
 				texel_color = blk->textureimg[WE].addr[(TEXTURE_WIDTH
-						* sprite_offset_y) + sprite_offset_x];
+						* texture_offset_y) + texture_offset_x];
 				my_mlx_pixelput(blk, i, y, texel_color);
 			}
 			else if (blk->ray[i].is_hit_vertical
 				&& blk->ray[i].is_ray_face_left)
 			{
 				texel_color = blk->textureimg[EA].addr[(TEXTURE_WIDTH
-						* sprite_offset_y) + sprite_offset_x];
+						* texture_offset_y) + texture_offset_x];
 				my_mlx_pixelput(blk, i, y, texel_color);
 			}
 			else if (!blk->ray[i].is_hit_vertical
 				&& blk->ray[i].is_ray_face_up)
 			{
 				texel_color = blk->textureimg[NO].addr[(TEXTURE_WIDTH
-						* sprite_offset_y) + sprite_offset_x];
+						* texture_offset_y) + texture_offset_x];
 				my_mlx_pixelput(blk, i, y, texel_color);
 			}
 			else if (!blk->ray[i].is_hit_vertical
 				&& blk->ray[i].is_ray_face_down)
 			{
 				texel_color = blk->textureimg[SO].addr[(TEXTURE_WIDTH
-						* sprite_offset_y) + sprite_offset_x];
+						* texture_offset_y) + texture_offset_x];
 				my_mlx_pixelput(blk, i, y, texel_color);
 			}
 			y++;
