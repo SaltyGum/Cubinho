66e78c35c3c33b51aa4b29432e5efb067023f1e5
Daniel Vargas <daniel_v93@hotmail.com>
2023-04-18 07:27:57 -0300

    Fallen_STRAFE

--- move.c
+++ move.c
@@ -6,7 +6,7 @@
 /*   By: dvargas < dvargas@student.42.rio>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 09:14:14 by dvargas           #+#    #+#             */
-/*   Updated: 2023/04/17 16:47:09 by jeluiz4          ###   ########.fr       */
+/*   Updated: 2023/04/18 07:24:54 by dvargas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,23 @@ int	move_is_valid(t_cub3d *blk, float move_x, float move_y)
 	}
 	return (0);
 }
+
+int	strafe(t_cub3d *data, int x)
+{
+    float move_step;
+    float angle;
+    double move_x;
+	double move_y;
+
+    data->player.rotation_angle += data->player.turn_direction * data->player.walk_speed;
+
+    move_step = x * data->player.walk_speed;
+    angle = data->player.rotation_angle + (3 * PI / 2);
+    move_x = data->player.x - cos(angle) * move_step;
+    move_y = data->player.y - sin(angle) * move_step;
+	return (move_is_valid(data, move_x, move_y));
+}
+
 
 int	move_to_front(t_cub3d *blk)
 {
@@ -70,5 +87,9 @@ int	move_player(t_cub3d *blk)
 		move += move_to_front(blk);
 	if (blk->player.walk_direction == -1)
 		move += move_to_back(blk);
+	if (blk->player.walk_direction == 2)
+		move += strafe(blk, -1);
+	if (blk->player.walk_direction == -2)
+		move += strafe(blk, 1);
 	return (move);
 }
